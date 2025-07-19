7e5410342a9470a080a81d101525e8a971f9e9a1
Danialanya10 <denisalanyaboza50@gmail.com>
2025-07-19 13:49:36 -0500

    Proyecto completo para tarjeta de perfil

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-document.getElementById('cambiarColor').addEventListener('click', () => {
-  document.body.style.backgroundColor = '#ffeeba';
+document.getElementById("cambiarColor").addEventListener("click", () => {
+  const colors = ["#ffeeba", "#d4edda", "#cce5ff", "#f8d7da"];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  document.body.style.backgroundColor = randomColor;
 });
